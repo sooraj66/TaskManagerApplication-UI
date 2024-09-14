b03289e656a91429fc4cb4f0b1dc4d42b061c225
Sooraj Sundaran <soorajsundaran66@gmail.com>
2024-09-14 15:32:19 +0530

    add task complete

--- src/components/task/add-task/AddTask.jsx
+++ src/components/task/add-task/AddTask.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { TaskService } from '../../../services/task.service';
 
-function AddTask() {
-    
+function AddTask({ handleClose }) {
+  const taskService = TaskService.instance;
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const createTask = async () => {
+    try {
+      const tasks = await taskService.createTask({ title, description });
+      handleClose(true);
+    }
+    catch (error) {
+      console.log(error);
+      handleClose();
+    }
+  }
 
   return (
-    <div>AddTask</div>
-  )
+    <>
+      <Modal show={true} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <>
+            <div className="input-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter Task title"
+                required
+              />
+
+              <label htmlFor="decription">Description</label>
+              <input
+                type="text"
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Enter Task description"
+                required
+              />
+            </div>
+            <button type="button" className="register-btn" onClick={createTask}>Save</button>
+          </>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
-export default AddTask
+export default AddTask;
--- src/components/task/task-container/TaskContainer.jsx
+++ src/components/task/task-container/TaskContainer.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import './TaskContainer.css'
 import TaskList from "../task-list/TaskList"
 import Navbar from '../../navbar/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function TaskContainer() {
-
 
     const searchTasks = (query) => {
         // setFilteredTasks(tasks.filter(task => task.title.toLowerCase().includes(query.toLowerCase())));
@@ -19,10 +17,10 @@ function TaskContainer() {
         //     setFilteredTasks(tasks.filter(task => task.status === status));
         // }
     };
+
     return (
         <div className="task-list-container">
             <Navbar onSearch={searchTasks} onFilter={filterTasks} />
-            <button className='btn btn-primary mt-1' onClick={() => addTask('New Task')}>Add Task</button>
             <TaskList />
         </div>
     )
