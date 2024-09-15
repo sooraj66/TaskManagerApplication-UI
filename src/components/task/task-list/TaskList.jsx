cde36b254e6f88a87afc8b940e49493dfc63a9e2
Sooraj Sundaran <soorajsundaran66@gmail.com>
2024-09-15 20:28:22 +0530

    style addition

--- src/components/task/task-list/TaskList.jsx
+++ src/components/task/task-list/TaskList.jsx
@@ -17,37 +17,36 @@ const TaskList = () => {
     const [filterValues, setFilterValues] = useState({
         searchVal: '',
         statusFilterVal: 'all'
-    })
+    });
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
 
     useEffect(() => {
         getAllTasks();
-    }, []);
+    }, [currentPage, filterValues]);
 
-    useEffect(() => {
-        let queryParams = [];
-        if (filterValues.searchVal) {
-            queryParams.push(`q=${filterValues.searchVal.toLowerCase()}`);
-        }
-        if (filterValues.statusFilterVal !== 'all') {
-            const status = filterValues.statusFilterVal === 'completed' ? 'True' : 'False';
-            queryParams.push(`status=${status}`);
-        }
-
-        const queryString = queryParams.length ? `?${queryParams.join('&')}` : '';
-        getAllTasks(queryString);
-    }, [filterValues])
-
-    const getAllTasks = async (qryString) => {
+    const getAllTasks = async () => {
         try {
-            let url = `${Config.API_BASE_URL}/alltasks/`
-            if (qryString)
-                url = `${Config.API_BASE_URL}/alltasks/${qryString}`;
+            let queryParams = [];
+            if (filterValues.searchVal) {
+                queryParams.push(`q=${filterValues.searchVal.toLowerCase()}`);
+            }
+            if (filterValues.statusFilterVal !== 'all') {
+                const status = filterValues.statusFilterVal === 'completed' ? 'True' : 'False';
+                queryParams.push(`status=${status}`);
+            }
+            queryParams.push(`page=${currentPage}`);
+            const queryString = queryParams.length ? `?${queryParams.join('&')}` : '';
+
+            const url = `${Config.API_BASE_URL}/alltasks/${queryString}`;
             const result = await axios.get(url, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             });
-            setTasks(result?.data);
+
+            setTasks(result?.data?.tasks || []);
+            setTotalPages(result?.data?.total_pages || 1);
         }
         catch (err) {
             if (err?.status === 401) {
@@ -70,61 +69,84 @@ const TaskList = () => {
     const handleFilterClick = (selectedOption) => {
         switch (selectedOption) {
             case 'all':
-                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'all' }))
+                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'all' }));
                 break;
             case 'completed':
-                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'completed' }))
+                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'completed' }));
                 break;
             case "pending":
-                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'pending' }))
+                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'pending' }));
                 break;
         }
         setIsTaskFilterDropdownOpen(false);
+        setCurrentPage(1);
     }
 
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    }
 
-    return <div className="task-list-container">
-        <div className="d-flex justify-content-end gap-2 mb-3 task-list-main">
-            <input
-                type="text"
-                className="search-bar form-control"
-                style={{ width: 'inherit' }}
-                placeholder="Search Tasks..."
-                onChange={(e) => setFilterValues(prevVal => ({ ...prevVal, searchVal: e.target.value }))}
-            />
-            <div className="dropdown">
-                <button
-                    className="dropdown-btn"
-                    onClick={() => setIsTaskFilterDropdownOpen(!isTaskFilterDropdownOpen)}>
-                    Filter Status
-                </button>
-                {isTaskFilterDropdownOpen && (
-                    <div className="dropdown-content">
-                        <button className={filterValues.statusFilterVal == 'all' ? 'active' : ''} onClick={() => handleFilterClick('all')}>All</button>
-                        <button className={filterValues.statusFilterVal == 'completed' ? 'active' : ''} onClick={() => handleFilterClick('completed')}>Completed</button>
-                        <button className={filterValues.statusFilterVal == 'pending' ? 'active' : ''} onClick={() => handleFilterClick('pending')}>Pending</button>
-                    </div>
+    return (
+        <div className="task-list-container">
+            <div className="d-flex justify-content-end gap-2 mb-3 task-list-main">
+                <input
+                    type="text"
+                    className="search-bar form-control"
+                    style={{ width: 'inherit' }}
+                    placeholder="Search Tasks..."
+                    onChange={(e) => setFilterValues(prevVal => ({ ...prevVal, searchVal: e.target.value }))}
+                />
+                <div className="dropdown">
+                    <button
+                        className="dropdown-btn"
+                        onClick={() => setIsTaskFilterDropdownOpen(!isTaskFilterDropdownOpen)}>
+                        Filter Status
+                    </button>
+                    {isTaskFilterDropdownOpen && (
+                        <div className="dropdown-content">
+                            <button className={filterValues.statusFilterVal === 'all' ? 'active' : ''} onClick={() => handleFilterClick('all')}>All</button>
+                            <button className={filterValues.statusFilterVal === 'completed' ? 'active' : ''} onClick={() => handleFilterClick('completed')}>Completed</button>
+                            <button className={filterValues.statusFilterVal === 'pending' ? 'active' : ''} onClick={() => handleFilterClick('pending')}>Pending</button>
+                        </div>
+                    )}
+                </div>
+                <button className="btn btn-primary" onClick={handleShowAddTaskPopup}>Add Task</button>
+            </div>
+
+            <div className="row task-list">
+                {tasks.length === 0 ? (
+                    <div>No Tasks</div>
+                ) : (
+                    tasks.map(task => (
+                        <div className="col" key={task.id}>
+                            <TaskItem
+                                task={task}
+                                refetch={getAllTasks}
+                            />
+                        </div>
+                    ))
                 )}
             </div>
-            <button className="btn btn-primary" onClick={handleShowAddTaskPopup}>Add Task</button>
+
+            <div className="pagination-controls">
+                <button
+                    className="btn btn-secondary"
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}>
+                    Previous
+                </button>
+                <span style={{ lineHeight: 2 }}>{`Page ${currentPage} of ${totalPages}`}</span>
+                <button
+                    className="btn btn-secondary"
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}>
+                    Next
+                </button>
+            </div>
+
+            {showAddPopup && <AddOrEditTask handleClose={handleHideAddTaskPopup} />}
         </div>
-
-        <div className="row task-list">
-            {tasks.length === 0 ?
-                <div>No Tasks</div> :
-                tasks.map(task => (
-                    <div className="col" key={task.id}>
-                        <TaskItem
-                            task={task}
-                            refetch={getAllTasks}
-                        />
-                    </div>
-                ))}
-        </div>
-
-        {showAddPopup && <AddOrEditTask handleClose={handleHideAddTaskPopup} />}
-    </div>
-
+    );
 };
 
 export default TaskList;
