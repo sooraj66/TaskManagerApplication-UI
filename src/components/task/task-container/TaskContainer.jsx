3b72e737beb3417f1a82c68b1cc75ed2201b5cc3
Sooraj Sundaran <soorajsundaran66@gmail.com>
2024-09-15 21:42:23 +0530

    code arragement

--- src/components/task/task-container/TaskContainer.jsx
+++ src/components/task/task-container/TaskContainer.jsx
@@ -1,16 +1,141 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './TaskContainer.css'
-import TaskList from "../task-list/TaskList"
+import TaskList from "../task-list/TaskList";
 import Navbar from '../../navbar/Navbar';
+import AddOrEditTask from '../add-or-edit-task/AddOrEditTask';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import '../../navbar/Navbar.css';
+import Config from '../../../Config';
 
 
 function TaskContainer() {
+    const token = localStorage.getItem('access_token');
+    const navigate = useNavigate();
+
+    const [isTaskFilterDropdownOpen, setIsTaskFilterDropdownOpen] = useState(false);
+    const [tasks, setTasks] = useState([]);
+    const [showAddPopup, setShowAddPopup] = useState(false);
+    const [filterValues, setFilterValues] = useState({
+        searchVal: '',
+        statusFilterVal: 'all'
+    });
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    useEffect(() => {
+        getAllTasks();
+    }, [currentPage, filterValues]);
+
+    const getAllTasks = async () => {
+        try {
+            let queryParams = [];
+            if (filterValues.searchVal) {
+                queryParams.push(`q=${filterValues.searchVal.toLowerCase()}`);
+            }
+            if (filterValues.statusFilterVal !== 'all') {
+                const status = filterValues.statusFilterVal === 'completed' ? 'True' : 'False';
+                queryParams.push(`status=${status}`);
+            }
+            queryParams.push(`page=${currentPage}`);
+            const queryString = queryParams.length ? `?${queryParams.join('&')}` : '';
+
+            const url = `${Config.API_BASE_URL}/alltasks/${queryString}`;
+            const result = await axios.get(url, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            setTasks(result?.data?.tasks || []);
+            setTotalPages(result?.data?.total_pages || 1);
+        }
+        catch (err) {
+            if (err?.status === 401) {
+                navigate('/login')
+            }
+        }
+    }
+
+    const handleShowAddTaskPopup = () => {
+        setShowAddPopup(true)
+    }
+
+    const handleHideAddTaskPopup = (isTaskCreated = false) => {
+        setShowAddPopup(false);
+        if (isTaskCreated) {
+            getAllTasks();
+        }
+    }
+
+    const handleFilterClick = (selectedOption) => {
+        switch (selectedOption) {
+            case 'all':
+                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'all' }));
+                break;
+            case 'completed':
+                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'completed' }));
+                break;
+            case "pending":
+                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'pending' }));
+                break;
+        }
+        setIsTaskFilterDropdownOpen(false);
+        setCurrentPage(1);
+    }
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    }
 
     return (
         <>
             <Navbar />
             <div className="task-list-container">
-                <TaskList />
+                <div className="d-flex justify-content-end gap-2 mb-3 task-list-main">
+                    <input
+                        type="text"
+                        className="search-bar form-control"
+                        style={{ width: 'inherit' }}
+                        placeholder="Search Tasks..."
+                        onChange={(e) => setFilterValues(prevVal => ({ ...prevVal, searchVal: e.target.value }))}
+                    />
+                    <div className="dropdown">
+                        <button
+                            className="dropdown-btn"
+                            onClick={() => setIsTaskFilterDropdownOpen(!isTaskFilterDropdownOpen)}>
+                            Filter Status
+                        </button>
+                        {isTaskFilterDropdownOpen && (
+                            <div className="dropdown-content">
+                                <button className={filterValues.statusFilterVal === 'all' ? 'active' : ''} onClick={() => handleFilterClick('all')}>All</button>
+                                <button className={filterValues.statusFilterVal === 'completed' ? 'active' : ''} onClick={() => handleFilterClick('completed')}>Completed</button>
+                                <button className={filterValues.statusFilterVal === 'pending' ? 'active' : ''} onClick={() => handleFilterClick('pending')}>Pending</button>
+                            </div>
+                        )}
+                    </div>
+                    <button className="btn btn-primary" onClick={handleShowAddTaskPopup}>Add Task</button>
+                </div>
+
+                {<TaskList tasks={tasks} refetch={getAllTasks} />}
+
+                <div className="pagination-controls">
+                    <button
+                        className="btn btn-secondary"
+                        onClick={() => handlePageChange(currentPage - 1)}
+                        disabled={currentPage === 1}>
+                        Previous
+                    </button>
+                    <span style={{ lineHeight: 2 }}>{`Page ${currentPage} of ${totalPages}`}</span>
+                    <button
+                        className="btn btn-secondary"
+                        onClick={() => handlePageChange(currentPage + 1)}
+                        disabled={currentPage === totalPages}>
+                        Next
+                    </button>
+                </div>
+
+                {showAddPopup && <AddOrEditTask handleClose={handleHideAddTaskPopup} />}
             </div>
         </>
     )
--- src/components/task/task-list/TaskList.jsx
+++ src/components/task/task-list/TaskList.jsx
@@ -1,150 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import TaskItem from '../task-item/Taskitem';
-import AddOrEditTask from '../add-or-edit-task/AddOrEditTask';
 import "./TaskList.css";
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import '../../navbar/Navbar.css';
-import Config from '../../../Config';
 
-const TaskList = () => {
-    const token = localStorage.getItem('access_token');
-    const navigate = useNavigate();
-
-    const [isTaskFilterDropdownOpen, setIsTaskFilterDropdownOpen] = useState(false);
-    const [tasks, setTasks] = useState([]);
-    const [showAddPopup, setShowAddPopup] = useState(false);
-    const [filterValues, setFilterValues] = useState({
-        searchVal: '',
-        statusFilterVal: 'all'
-    });
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-
-    useEffect(() => {
-        getAllTasks();
-    }, [currentPage, filterValues]);
-
-    const getAllTasks = async () => {
-        try {
-            let queryParams = [];
-            if (filterValues.searchVal) {
-                queryParams.push(`q=${filterValues.searchVal.toLowerCase()}`);
-            }
-            if (filterValues.statusFilterVal !== 'all') {
-                const status = filterValues.statusFilterVal === 'completed' ? 'True' : 'False';
-                queryParams.push(`status=${status}`);
-            }
-            queryParams.push(`page=${currentPage}`);
-            const queryString = queryParams.length ? `?${queryParams.join('&')}` : '';
-
-            const url = `${Config.API_BASE_URL}/alltasks/${queryString}`;
-            const result = await axios.get(url, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-
-            setTasks(result?.data?.tasks || []);
-            setTotalPages(result?.data?.total_pages || 1);
-        }
-        catch (err) {
-            if (err?.status === 401) {
-                navigate('/login')
-            }
-        }
-    }
-
-    const handleShowAddTaskPopup = () => {
-        setShowAddPopup(true)
-    }
-
-    const handleHideAddTaskPopup = (isTaskCreated = false) => {
-        setShowAddPopup(false);
-        if (isTaskCreated) {
-            getAllTasks();
-        }
-    }
-
-    const handleFilterClick = (selectedOption) => {
-        switch (selectedOption) {
-            case 'all':
-                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'all' }));
-                break;
-            case 'completed':
-                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'completed' }));
-                break;
-            case "pending":
-                setFilterValues(prevVal => ({ ...prevVal, statusFilterVal: 'pending' }));
-                break;
-        }
-        setIsTaskFilterDropdownOpen(false);
-        setCurrentPage(1);
-    }
-
-    const handlePageChange = (newPage) => {
-        setCurrentPage(newPage);
-    }
+const TaskList = ({ tasks, refetch }) => {
 
     return (
-        <div className="task-list-container">
-            <div className="d-flex justify-content-end gap-2 mb-3 task-list-main">
-                <input
-                    type="text"
-                    className="search-bar form-control"
-                    style={{ width: 'inherit' }}
-                    placeholder="Search Tasks..."
-                    onChange={(e) => setFilterValues(prevVal => ({ ...prevVal, searchVal: e.target.value }))}
-                />
-                <div className="dropdown">
-                    <button
-                        className="dropdown-btn"
-                        onClick={() => setIsTaskFilterDropdownOpen(!isTaskFilterDropdownOpen)}>
-                        Filter Status
-                    </button>
-                    {isTaskFilterDropdownOpen && (
-                        <div className="dropdown-content">
-                            <button className={filterValues.statusFilterVal === 'all' ? 'active' : ''} onClick={() => handleFilterClick('all')}>All</button>
-                            <button className={filterValues.statusFilterVal === 'completed' ? 'active' : ''} onClick={() => handleFilterClick('completed')}>Completed</button>
-                            <button className={filterValues.statusFilterVal === 'pending' ? 'active' : ''} onClick={() => handleFilterClick('pending')}>Pending</button>
-                        </div>
-                    )}
-                </div>
-                <button className="btn btn-primary" onClick={handleShowAddTaskPopup}>Add Task</button>
-            </div>
-
-            <div className="row  row-cols-1 row-cols-md-2 row-cols-lg-3 g-4  task-list">
-                {tasks.length === 0 ? (
-                    <div>No Tasks</div>
-                ) : (
-                    tasks.map(task => (
-                        <div className="col" key={task.id}>
-                            <TaskItem
-                                task={task}
-                                refetch={getAllTasks}
-                            />
-                        </div>
-                    ))
-                )}
-            </div>
-
-            <div className="pagination-controls">
-                <button
-                    className="btn btn-secondary"
-                    onClick={() => handlePageChange(currentPage - 1)}
-                    disabled={currentPage === 1}>
-                    Previous
-                </button>
-                <span style={{ lineHeight: 2 }}>{`Page ${currentPage} of ${totalPages}`}</span>
-                <button
-                    className="btn btn-secondary"
-                    onClick={() => handlePageChange(currentPage + 1)}
-                    disabled={currentPage === totalPages}>
-                    Next
-                </button>
-            </div>
-
-            {showAddPopup && <AddOrEditTask handleClose={handleHideAddTaskPopup} />}
+        <div className="row  row-cols-1 row-cols-md-2 row-cols-lg-3 g-4  task-list">
+            {tasks.length === 0 ? (
+                <div>No Tasks</div>
+            ) : (
+                tasks.map(task => (
+                    <div className="col" key={task.id}>
+                        <TaskItem
+                            task={task}
+                            refetch={refetch}
+                        />
+                    </div>
+                ))
+            )}
         </div>
     );
 };
